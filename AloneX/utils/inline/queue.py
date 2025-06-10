db22e751f9d03be84c83d1958a51e773a7e4ba3e
UniqeXD <shubhogarai730@gmail.com>
2025-06-11 00:34:29 +0530

    Update queue.py

--- AloneX/utils/inline/queue.py
+++ AloneX/utils/inline/queue.py
@@ -75,15 +75,7 @@ def aq_markup(_, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="• 𝐒ᴜᴘᴘᴏʀᴛ •", url=config.SUPPORT_CHAT
-            ),
-            InlineKeyboardButton(
-                text="• 𝐔ᴘᴅᴀᴛᴇs •", url=config.SUPPORT_CHANNEL
-            ),
-        ],
-        [
-            InlineKeyboardButton(
-                text="• 𝐃ᴇᴠᴏʟᴏᴘᴇʀ •", user_id=config.OWNER_ID,
+                text="⌯ 𝐒ᴜʙsᴄʀɪʙᴇ 𝐓ᴏ 𝐔ᴘᴅᴀᴛᴇs ⌯", url=config.SUPPORT_CHAT
             ),
         ],
         [InlineKeyboardButton(text="[✗]𝐂ʟᴏsᴇ[✗]", callback_data="close")],
